4cd9a3b0af08903222da349b6b2a45bd9feed5fe
Muhammed Uygun <muhammed.uygun@invicti.com>
2021-11-15 19:51:35 +0300

    New Updates Released

--- kodama.py
+++ kodama.py
@@ -1,23 +1,54 @@
-from flask import Flask, request, render_template_string, render_template, make_response
-from http.client import HTTPResponse
-
+from flask import Flask, request, render_template_string, render_template, redirect, make_response, url_for, flash
+from http.client import HTTPResponse, responses
 from io import BytesIO
+import os
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+from pathlib import Path
+
+
+
 
 # set the project root directory as the static folder, you can set others.
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, static_url_path='/static')
 
-@app.route('/test')
-def test():
-    return render_template('example.html')
+# set the root path for save uploaded static files
+UPLOAD_FOLDER = app.root_path +'\\static'
+ALLOWED_EXTENSIONS = {'txt', 'js', 'css', 'jpg', 'jpeg', 'png'}
 
+# set the upload folder for js, css file upload.
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/echo')
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        if "content" in request.form:
+            content = request.form['content']
+            with open("response.txt","w",newline='') as f:
+                f.write(content)
+            return render_template("index.html")
+        if 'file' not in request.files:
+            return redirect(request.url)
+        elif "file" in request.files:
+            file = request.files['file']
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+                return render_template("index.html")
+
+@app.route('/result')
 def echo():
-    
     rawResponse = ""
     with open("response.txt") as f:
         rawResponse = f.read()
-
     # Get content length of raw data, it may be different than the header value due to copy-paste of the response
     httpResponseParts = rawResponse.split("\n\n", 1)
     contentLength = len(httpResponseParts[1])
@@ -26,15 +57,11 @@ def echo():
     if indexContentLengthHeader > -1:
         endContentLengthHeader = rawResponse.find("\n", indexContentLengthHeader)
         parsedContentLength = int(rawResponse[indexContentLengthHeader + 15:endContentLengthHeader])
-
         if contentLength != parsedContentLength:
             rawResponse = rawResponse[:indexContentLengthHeader] + "Content-Length: " + str(contentLength) + rawResponse[endContentLengthHeader:] 
-       
-
     # Transfer-Encoding breaks the parse if the response is already assembled. So, if the header is present, remove it
     indexTEHeader = rawResponse.find("Transfer-Encoding")
     endOfTEHeader = rawResponse.find("\n", indexTEHeader)
-  
     rawResponseChecked = ""
     if indexTEHeader > -1:
         # Transfer Encoding Header found
@@ -47,15 +74,12 @@ def echo():
             rawResponseChecked = rawResponse
     else:
         rawResponseChecked = rawResponse
-    
-
     rawResponseEnc = rawResponseChecked.encode()
     class FakeSocket():
         def __init__(self, response_bytes):
             self._file = BytesIO(response_bytes)
         def makefile(self, *args, **kwargs):
             return self._file
-
     source = FakeSocket(rawResponseEnc)
     parsedResponse = HTTPResponse(source)
     parsedResponse.begin()
@@ -63,22 +87,20 @@ def echo():
     parsedResponse_headers = parsedResponse.getheaders()
     parsedResponse_status = parsedResponse.status
     parsedResponse.close()
-    
     response = make_response(parsedResponse_body)
+    response.set_cookie("mycookie", value="htegergergegf",secure=True,httponly=True)
     response.status_code = parsedResponse_status
     for header in parsedResponse_headers:
         print(header[0] + " " + header[1])
         response.headers[header[0]] = header[1]
-
     return response
+if __name__ == "__main__":
+    app.run(debug=True)  # app.run(host='0.0.0.0') for exposing the server to network
 
 
-# Set Headers for all responses, overwrite existing ones.
-#@app.after_request
-#def apply_caching(response):
+# Notes:
+
 #    response.headers["X-Custom-Header"] = "custom-value"
 #    response.headers["Server"] = "kodama"
 #    return response
-
-if __name__ == "__main__":
-    app.run() # app.run(host='0.0.0.0') for exposing the server to network
+#    response.set_cookie("mycookie2", value="htegergergegf")
