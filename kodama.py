0172045857d85bfe41b6929712b9073c9868281a
Muhammed Uygun <muhammed.uygun@invicti.com>
2021-11-15 22:31:22 +0300

    The desired changes have been made.

--- kodama.py
+++ kodama.py
@@ -22,7 +22,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "GET":
